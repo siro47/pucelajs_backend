e8d06c08665f9c4283aa22e4995f2e5006931438
sramirez <sramirez@solidgeargroup.com>
2019-10-29 08:41:57 +0100

    Added randomly generated strings

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const bodyparser = require('body-parser');
+const randomSentence = require('random-sentence');
+
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json({ limit: '10mb' }));
+
+setInterval(() => {
+    if (Math.random() <= 0.1)
+        io.emit("stdMessage", randomSentence({words: 5}))
+}, 20);
 
 app.post('/message', (req,res,next) => {
     io.emit("premMessage", req.body.msg);
